99224894c9104ee54d78cb4f5763636b8671c3f0
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-11-27 21:20:16 -0300

    Calculando Fatorial e Fibonacci

---
+++ 2024-11-27 - Laços de Repetição/fatorial.py
@@ -0,0 +1,7 @@
+'''
+   Fazer um programa que solicite ao usuário um valor inteiro e calcule
+   o fatorial desse número. O fatorial de um número é calculado pela
+   multiplicação desse número por todos os seus antecessores até o número 1.
+
+   Exemplo: 5! = 5 * 4 * 3 * 2 * 1 = 120
+'''
---
+++ 2024-11-27 - Laços de Repetição/fibonacci.py
@@ -0,0 +1,8 @@
+'''
+   Faça um programa que solicite ao usuário um valor inteiro positivo (n)
+   e imprima os n primeiros números da sequência de Fibonacci.
+
+   exemplo:
+   n = 5
+   0 1 1 2 3
+'''
