5e0c88177187e8d1a11be5ca26708f88bef18edf
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-11-29 19:28:16 -0300

    Exemplos de WHILE sem variável de controle

---
+++ 2024-11-29 - Laços de Repetição/exemplo_01.py
@@ -0,0 +1,5 @@
+'''
+   Fazer um programa que fique solicitando ao usuário para digitar um número
+   e informe se o número é par ou ímpar. O programa só deve encerrar quando o
+   usuário digitar o número 0.
+'''
---
+++ 2024-11-29 - Laços de Repetição/exemplo_02.py
@@ -0,0 +1,7 @@
+'''
+   Fazer um programa que fique lendo números inteiros do usuário 
+   até que ele digite 0.
+
+   Quando o usuário digitar 0, o programa deve imprimir a soma de 
+   todos os números digitados bem como a média aritmética.
+'''
