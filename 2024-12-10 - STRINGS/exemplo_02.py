d77aac5953995005b4904be348431d30199ca862
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2024-12-10 19:30:07 -0300

    Exemplos de Manipulação de STRINGS

---
+++ 2024-12-10 - STRINGS/exemplo_02.py
@@ -0,0 +1,5 @@
+'''
+   EXEMPLO 02:
+   Fazer um programa em que o usuário informe uma string e em seguida
+   o programa informe quantas vogais existem na string digitada
+'''
---
+++ 2024-12-10 - STRINGS/exemplo_03.py
@@ -0,0 +1,5 @@
+'''
+   EXEMPLO 03:
+   Fazer um programa em que o usuário informe uma string e em seguida
+   o programa informe quantas palavras existem na string digitada
+'''
