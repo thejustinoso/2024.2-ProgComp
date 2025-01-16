ede0517e2c5a848e43609aa413baf17551dbe568
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2025-01-15 21:23:51 -0300

    Aula introdutória de Listas

---
+++ 2025-01-15 - LISTAS/exemplo_01.py
@@ -0,0 +1,8 @@
+'''
+   EXEMPLO:
+   Fazer um programa que nomes de pessoas ao usuário. O programa deverá parar de
+   solicitar quando o usuário digitar FIM.
+   
+   Em seguida o programa deverá solicitar um nome de uma pessoa qualquer e 
+   informar se esse nome consta nos nomes digitados anterioremente ou não.
+'''
